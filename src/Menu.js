55cb5a07890d6dce8f83d1659aafd12583eaec7b
venomissionary <130535173+venomissionary@users.noreply.github.com>
2024-01-23 19:38:34 -0800

    Update
    - put back hard mode timer from 10000 to 45000

--- src/Menu.js
+++ src/Menu.js
@@ -71,7 +71,7 @@ class Menu extends Phaser.Scene {
             // hard mode
             game.settings = {
               spaceshipSpeed: 4,
-              gameTimer: 10000    
+              gameTimer: 45000    
             }
             this.sound.play('sfx-select')
             this.scene.start('playScene')    
